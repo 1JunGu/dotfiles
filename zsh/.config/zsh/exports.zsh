ee33043ae23ebc6b5506172cb81213f4b8b758fb
Jun Gu <kyuinkoo@mail.ustc.edu.cn>
2024-01-14 23:25:33 +0800

    1. Set $HOME/miniconda3 as the base conda env  2. `conda config --set auto_activate_base false` is too slow while starting zsh. Disable it.  Changes to be committed: 	modified:   zsh/.config/zsh/exports.zsh

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -25,16 +25,16 @@ setopt HIST_VERIFY              # Don't excute immediately upon history expansio
 # conda
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/jungu/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/Users/jungu/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/jungu/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/Users/jungu/miniconda3/bin:$PATH"
+        export PATH="$HOME/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<< 
-conda config --set auto_activate_base false
+# conda config --set auto_activate_base false ##time consuming large, disable it
