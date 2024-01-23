567106b98476086a344b7ea71df28ebec5f4e43e
Jun Gu <kyuinkoo@mail.ustc.edu.cn>
2024-01-23 20:10:21 +0800

    Add "lazygit"
     Changes to be committed:
    	modified:   zsh/.config/zsh/aliases.zsh

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -10,3 +10,6 @@ alias tree='lsd -l --group-directories-first --tree --depth=2'
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
+
+#lazygit
+alias lg='lazygit' #needed lazygit binary installed
