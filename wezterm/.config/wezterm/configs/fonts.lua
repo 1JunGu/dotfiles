be25b99229e552dbeb84d0cf4a167aa0092b7a36
JunGu <gj99@mail.ustc.edu.com>
2024-01-28 02:20:50 +0800

    Different fontsize in different platforms
     Changes to be committed:
    	modified:   wezterm/.config/wezterm/configs/fonts.lua

--- wezterm/.config/wezterm/configs/fonts.lua
+++ wezterm/.config/wezterm/configs/fonts.lua
@@ -1,12 +1,20 @@
 local wezterm = require "wezterm"
+local target = wezterm.target_triple
 
 local M = {}
 function M.append(config)
     local options = {
-	font_size = 14,
+	--font_size = 14,
 	font = wezterm.font("Fira Code", {weight="Medium"}), --or FiraCode Nerd Font Mono
 
     }
+    if target == "x86_64-pc-windows-msvc" then
+        options.font_size = 14
+
+    elseif target == "x86_64-apple-darwin" then
+        options.font_size = 15
+    end
+
     for key, value in pairs(options) do
         config[key] = value
     end
