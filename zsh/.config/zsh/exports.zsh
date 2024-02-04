ec96394b0ee07d348acc3dfe4e8bd668ad83244c
JunGu <gj99@mail.ustc.edu.com>
2024-02-05 00:54:10 +0800

    fix bug

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -15,11 +15,11 @@ Linux)
 Darwin)
 ##mac os
     #brew path
-    export PATH=/usr/local/bin:$PATH ;;
+    export PATH=/usr/local/bin:$PATH
     #nvm
     export NVM_DIR="$HOME/.nvm"
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"   # This loads nvm
+    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ;;  # This loads nvm bash_completion
 *)
     ;;
 esac
